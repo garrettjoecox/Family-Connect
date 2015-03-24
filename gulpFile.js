eb7b601c3eb750ae4feb4761850e1c18099a92ac
garrettjoecox <garrettjcox@gmail.com>
2015-03-23 23:40:17 -0700

    initial refactor commit

--- gulpFile.js
+++ gulpFile.js
@@ -1,42 +1,10 @@
 (function(){
   var gulp  = require('gulp');
-  var $     = require('gulp-load-plugins')({lazy:false});
 
-$.livereload();
-$.livereload.listen();
-
-var paths = {
-  index: './index.html',
-  root: '.',
-  html: './**/*.html',
-  scripts: './src/**/*.js',
-  styles: './src/**/*.css'
-}
-
-gulp.task('default', $.sequence('inject', 'server', 'watch'));
-gulp.task('server', startServer);
-gulp.task('watch', startWatch);
-gulp.task('inject', startInject)
+gulp.task('default', startServer);
 
 function startServer(){
   require('./server');
-
-}
-function startWatch(){
-  gulp.watch('./src/**/*.css', $.livereload.changed);
-  gulp.watch('./src/**/*.js', $.livereload.changed);
-  gulp.watch('./**/*.html', $.livereload.changed);
-}
-
-function startInject(){
-  var target  = gulp.src( paths.index );
-  var scripts = gulp.src( paths.scripts, {read:false} );
-  var styles  = gulp.src( paths.styles, {read:false} );
-
-  return target
-    .pipe( $.inject( scripts,  {relative:true}) )
-    .pipe( $.inject( styles,  {relative:true}) )
-    .pipe( gulp.dest( paths.root ) );
 }
 
 })();
--- server.js
+++ server.js
@@ -2,14 +2,9 @@
 
   'use strict';
   var express = require('express');
-  var reloader = require('connect-livereload')
   var app = express();
 
-
-
-  app.use(reloader());
-  app.use(express.static('./'));
-
+  app.use(express.static(__dirname));
 
   app.listen(9000, function(){
     console.log('App Listening on localhost:9000');
